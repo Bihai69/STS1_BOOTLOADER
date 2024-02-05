4b605b71dc33435c6c702c726f036cf568fa8917
Mbojor <mihaibojor@gmail.com>
2024-02-05 20:02:39 +0100

    Changed thigns

--- MakeAll.sh
+++ MakeAll.sh
@@ -4,10 +4,10 @@ make clean --directory=STS1_DEV_BOOTLOADER/
 make --directory=STS1_DEV_APP/
 make --directory=STS1_DEV_BOOTLOADER/
 
-python3 pad-bootloader.py
-cat write STS1_DEV_BOOTLOADER/build/STS1_DEV_BOOTLOADER.bin STS1_DEV_APP/build/STS1_DEV_APP.bin > firmware.bin
+#python3 pad-bootloader.py
+#cat write STS1_DEV_BOOTLOADER/build/STS1_DEV_BOOTLOADER.bin STS1_DEV_APP/build/STS1_DEV_APP.bin > firmware.bin
 
-st-flash write firmware.bin 0x8000000
+#st-flash write firmware.bin 0x8000000
 
-#st-flash write STS1_DEV_APP/build/STS1_DEV_APP.bin 0x8008000
-#st-flash write STS1_DEV_BOOTLOADER/build/STS1_DEV_BOOTLOADER.bin 0x8000000
+st-flash write STS1_DEV_APP/build/STS1_DEV_APP.bin 0x8008000
+st-flash write STS1_DEV_BOOTLOADER/build/STS1_DEV_BOOTLOADER.bin 0x8000000
